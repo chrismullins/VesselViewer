8678cf0c7b5bcb93103ae6af4f8e1e6e0d33a66f
Christopher Mullins <christopher.mullins@kitware.com>
2013-04-23 15:45:04 -0300

    Add a different colorMap

--- VesselViewer.cxx
+++ VesselViewer.cxx
@@ -12,6 +12,8 @@
 #include <vtkActor.h>
 #include <vtkProperty.h>
 #include <vtkObjectFactory.h>
+#include <vtkColorSeries.h>
+#include <vtkColorTransferFunction.h>
 
 // headers needed for this example
 #include <vtkParticleReader.h>
@@ -78,14 +80,48 @@ int main(int argc, char* argv[])
       vtkSmartPointer<vtkPolyDataMapper>::New();
    mapper->SetInputConnection(reader->GetOutputPort());
    std::cout << "number of pieces: " << mapper->GetNumberOfPieces() << std::endl;
-   mapper->SetScalarRange(3, 9);
+   //mapper->SetScalarRange(3, 9);
 
+  /**/
+  // Build a lookup table
+  double scalarRange[2];
+  scalarRange[0] = 0.0;
+  scalarRange[1] = 2.0;
+  vtkSmartPointer<vtkColorSeries> colorSeries = 
+    vtkSmartPointer<vtkColorSeries>::New();
+  colorSeries->SetColorScheme(4);
+  //colorSeries->SetColorSchemeByName("BREWER_DIVERGING_PURPLE_ORANGE_11");
+  std::cout << "Using color scheme #: "
+            << colorSeries->GetColorScheme() << " is "
+            << colorSeries->GetColorSchemeName() << std::endl;
+ 
+  vtkSmartPointer<vtkColorTransferFunction> lut = 
+    vtkSmartPointer<vtkColorTransferFunction>::New();
+  lut->SetColorSpaceToDiverging();
+ 
+  // Use a color series to create a transfer function
+  int numColors = colorSeries->GetNumberOfColors();
+  for (int i = 0; i < numColors; i++)
+    {
+      vtkColor3ub color = colorSeries->GetColor(i);
+    double dColor[3];
+    dColor[0] = static_cast<double> (color[0]) / 255.0;
+    dColor[1] = static_cast<double> (color[1]) / 255.0;
+    dColor[2] = static_cast<double> (color[2]) / 255.0;
+    double t = scalarRange[0] + (scalarRange[1] - scalarRange[0])
+      / (numColors - 1) * i;
+    lut->AddRGBPoint(t, dColor[0], dColor[1], dColor[2]);
+    }
+ 
+  mapper->SetLookupTable(lut);
+  /**/
    vtkSmartPointer<vtkActor> actor =
       vtkSmartPointer<vtkActor>::New();
 
    actor->SetMapper(mapper);
    actor->GetProperty()->SetPointSize(2);
 
+   
    vtkSmartPointer<vtkRenderer> renderer =
       vtkSmartPointer<vtkRenderer>::New();
    vtkSmartPointer<vtkRenderWindow> renderWindow =
